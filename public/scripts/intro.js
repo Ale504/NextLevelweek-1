125d5d00015ee88cea93a973446b95c1e1d7b1db
ale504 <luiz504@hotmail.com>
2020-06-18 10:19:23 -0300

    first commit

---
+++ public/scripts/intro.js
@@ -0,0 +1,70 @@
+//document.write ("Hello")
+
+// variaveis, tipos de dados
+//let myvar = "hi"
+//const myconst = "he"
+
+//document.write(myconst + myvar)
+
+
+// string
+//const s1 = "Isoo é uma string"
+//const s2 = 'Isso também é um string'
+//const s3 = `Isso é uma strin também`
+
+//number
+//const n1 = 24
+//const n2 = 4
+//const n3 = 1
+//document.write(n1 + n2 + n3)
+
+//boolean - true ou false
+//const bTrue = true
+//const bFalse = false
+//document.write(bTrue)
+
+//objeto possuem propriedades e funcionalidade
+//const pessoa = {
+//  altura: "1,80m",
+//  idade: 24,
+//  solteiro: true,
+//  correr(){
+//    return "run forest"
+//  }
+// }
+
+//document.write(pessoa.correr())
+
+// Array - vetores
+//const colecaoDeBolinhas = ["blue", "green", 1, {name: "My name"}]
+
+//document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+
+// registrar função
+//function sayMyName(name) {
+  
+//  document.write(name)
+//}
+
+// // executar
+//SayMyName("fulano")
+//SayMyName("Valeska")
+//SayMyName("kyam")
+
+//const notaFinal = 4
+
+//if( notaFinal< 5 ) {
+//  document.write("Reprovado")
+  // caminho 1
+//} else {
+//  document.write("Aprovado")
+  // caminho 2
+//}
+
+// loop repetições
+
+//for (i = 0; i < 10; i++) {
+//  document.write(`<p>${i}</p>`)
+//}
